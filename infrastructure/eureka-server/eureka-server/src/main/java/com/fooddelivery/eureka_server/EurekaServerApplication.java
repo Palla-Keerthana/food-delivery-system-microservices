ec996ac52eeb1c0b2f42e05b0bc3d8ccbe90b377
manasa-380 <2499380@cognizant.com>
2026-06-08 09:12:52 +0530

    Final working services

--- infrastructure/eureka-server/eureka-server/src/main/java/com/fooddelivery/eureka_server/EurekaServerApplication.java
+++ infrastructure/eureka-server/eureka-server/src/main/java/com/fooddelivery/eureka_server/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.fooddelivery.eureka_server;   // ✅ no hyphen
+package com.fooddelivery.eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
